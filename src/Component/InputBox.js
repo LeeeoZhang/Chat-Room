417ed7689b90180a64edbe69eb6e921593c6340a
leo <leozhang621@gmail.com>
2018-04-07 22:21:32 +0800

    add InputBox

--- src/Component/InputBox.js
+++ src/Component/InputBox.js
@@ -2,14 +2,17 @@ import React from 'react'
 import './InputBox.css'
 
 
-function InputBox (){
-    return (
-        <div className="input-box-wrap">
-            <div className="input-box">
-                <textarea name="chat-input" id="input" placeholder="Input Your Wrod!"/>
+class InputBox extends React.Component {
+    render () {
+        return (
+            <div className="input-box-wrap">
+                <div className="input-box">
+                    <textarea name="chat-input" id="input" placeholder="Press Enter"/>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
 
 export default InputBox
